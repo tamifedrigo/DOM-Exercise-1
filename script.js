596f16a6d3baf2462e608bce6ed710634ae8fdbd
Tami Fedrigo <tamifedrigo@tamis-mbp.lan>
2021-05-12 18:38:22 -0400

    First Commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function main() {
+    const growMe = document.getElementById("grow-me");
+    growMe.classList.add("big");
+
+const shrinkMe = document.getElementById('shrink-me');
+console.log(shrinkMe);
+shrinkMe.classList.remove("big");
+
+const listItems = document.querySelectorAll('.list-item')
+console.log(listItem);
+
+const link = document.querySelector('.link');
+console.log(link) 
+link.innerTet = "somewhere";
+}
